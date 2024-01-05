9d83bf6aa7e7f99755f62e2f03783d7d5716fe26
stcrl <insp.anv.43@gmail.com>
2024-01-05 16:36:35 -0300

    update client

--- dsClient/backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ dsClient/backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -11,7 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repositories.ClientRepository;
-import com.devsuperior.dsclient.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -29,9 +31,10 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+		Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
+	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
 		entity.setName(dto.getName());
@@ -41,6 +44,22 @@ public class ClientService {
 		entity.setChildren(dto.getChildren());
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+		
+	}
+	@Transactional
+	public ClientDTO update(Long id,ClientDTO dto) {
+		try {
+		Client entity = repository.getReferenceById(id);
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id not found" + id);
+		}
 		
 	}
 }
